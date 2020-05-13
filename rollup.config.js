20e44cb7cb2d8cd8a7b3367204edd9f1a69aa8e3
jul <julien.wist@correounivalle.edu.co>
2020-05-13 16:50:43 -0500

    fix: add cheminfo-build and cheminfo-publish

--- rollup.config.js
+++ rollup.config.js
@@ -1,7 +1,12 @@
-export default {
+const config = {
   input: 'src/index.js',
   output: {
+    file: 'lib/index.js',
     format: 'cjs',
-    file: 'lib/index.js'
-  }
+    exports: 'named',
+  },
+  external: [],
 };
+
+export default config;
+
